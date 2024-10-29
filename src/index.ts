e78ef1f9327dc3501af5e88b07883fc9b1119fc3
MickaelJoseph <mickaelandriana06@gmail.com>
2024-10-29 15:48:11 +0300

    No change

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express , {Request,Response} from 'express'
+import express  from 'express'
 import { PrismaClient } from '@prisma/client'
 import routes from './routes'
 import cors from "cors"
