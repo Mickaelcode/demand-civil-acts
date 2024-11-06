bba964b1859b20f8dc6a3e20777449fc66a2948e
MickaelJoseph <mickaelandriana06@gmail.com>
2024-11-06 13:00:01 +0300

    read act

--- src/controllers/acte.ts
+++ src/controllers/acte.ts
@@ -1,31 +1,46 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 import prisma from "..";
 import fs from 'fs'
- 
-export const createAct = async (req:Request ,res:Response) =>{
+
+export const createAct = async (req: Request, res: Response) => {
     const files = req.files as Express.Multer.File[]
-    try{
-        
-        const fileActe = files.length>1? files.map(fic=>fic.path).join('/') : files[0].path
-        console.log({...req.body,fileActe});
-        
+    try {
+
+        const fileActe = files.length > 1 ? files.map(fic => fic.path).join('/') : files[0].path
+        console.log({ ...req.body, fileActe });
+
         const acte = await prisma.acte.create({
-            data:{
+            data: {
                 ...req.body,
                 fileActe
             }
         })
         const msg = "acte created successfully!"
-        res.status(200).json({msg,acte})
+        res.status(200).json({ msg, acte })
         return
-    }catch(err){
-        files.forEach(file =>{
-            fs.unlink(file.path,(err)=>{
+    } catch (err) {
+        files.forEach(file => {
+            fs.unlink(file.path, (err) => {
                 console.log(err);
             })
         })
         const msg = "have error !"
-        res.status(500).json({msg,err})
+        res.status(500).json({ msg, err })
         return
     }
 }
+
+export const readAct = async (req:Response,res:Response) =>{
+    try{
+        let msg = ""
+        const act = await prisma.acte.findMany()
+        if (!act){
+            msg="Empty!"
+            res.status(200).json({msg})
+            return
+        }
+        
+    }catch(err){
+
+    }
+} 
