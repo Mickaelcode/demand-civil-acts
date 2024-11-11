617d0518d091b16614fc6ae2f1b0d6c7fb5a3e9a
MickaelJoseph <mickaelandriana06@gmail.com>
2024-11-11 14:01:11 +0300

    delete demande

--- src/routes/demands.ts
+++ src/routes/demands.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { security_admin } from "../middlewares/security_admin";
 import { upload } from "../middlewares/upload";
-import { createDemand, readDemand, updateDemand } from "../controllers/demands";
+import { createDemand, deleteDemand, readDemand, updateDemand } from "../controllers/demands";
 
 const demandRoute = Router ()
 
@@ -11,7 +11,8 @@ const demandRoute = Router ()
 
 demandRoute.post('/create',security_admin,upload.array('files'),createDemand)
 demandRoute.get('/lists',security_admin,readDemand)
-demandRoute.put('/update',upload.array('files'),updateDemand)
+demandRoute.put('/update',security_admin,upload.array('files'),updateDemand)
+demandRoute.delete('/delete',security_admin,deleteDemand)
 
 
 export default demandRoute
