358a9f72006c00b54d77070d0b08355d794bca63
MickaelJoseph <mickaelandriana06@gmail.com>
2024-11-18 16:19:16 +0300

    solve big issue for update demande

--- src/controllers/demands.ts
+++ src/controllers/demands.ts
@@ -1,19 +1,16 @@
 import { Request, Response } from "express";
-import prisma ,{PORT} from "..";
-import fs from "fs"
+import prisma from "..";
 
 export const createDemand = async (req: Request, res: Response) => {
     const files = req.files as Express.Multer.File[]
     try {
         let msg = ""
-        const attachment = files.length <= 1 ? false : files.map(fic => fic.path).join('~')
+        const attachment = files.length <= 1 ? false : files.map(fic => fic.buffer.toString('base64'))
         if (!attachment ) {
             msg = "file not accepted"
             res.status(401).json({ msg })
             return
-        }
-        console.log({...req.body,attachment});
-        
+        }        
         const demand = await prisma.demand.create({
             data: {
                 ...req.body,
@@ -25,12 +22,6 @@ export const createDemand = async (req: Request, res: Response) => {
         res.status(200).json({ msg, demand })
         return
     } catch (err) {
-        files.forEach(file =>{
-            fs.unlink(file.path,(err) =>{
-                console.log(err);
-                
-            })
-        })
         res.status(500).json({ err })
         return
 
@@ -46,9 +37,10 @@ export const readDemand = async (req: Request, res: Response) => {
             res.status(200).json({ msg })
             return
         }
-        let data = demand.map(a => ({ ...a, attachment: a.attachment.split('~').map(att => process.env.API_URL!+PORT+'/image/'+att.replace('files/','') )}))
         msg = "here list of demand"
-        res.status(200).json({ msg, data })
+        console.log({...demand});
+        
+        res.status(200).json({ msg, demand })
         return
     } catch (err) {
         res.status(500).json({ err })
@@ -78,40 +70,88 @@ export const deleteDemand = async (req:Request,res:Response) =>{
 }
 
 export const updateDemand = async (req:Request,res:Response) =>{
+    const files  = req.files as Express.Multer.File[]
+    const {
+        
+        idDemande  ,
+        actDemand ,
+        emailAdmin,
+        emailUser ,
+        numActe,
+      
+        province  ,
+        commune  ,
+        name     ,
+        firstName  ,
+        dateOfBirth ,
+        placeOfBirth ,
+        
+      
+        status ,
+        paid,
+
+    } = req.body
     try{
-        console.log(req.body);
         
         let msg = ""
-        const id = req.body.id
-        const files  = req.files as Express.Multer.File[]
-        const attachment = files.length <= 1 ? false : files.map(fic => fic.path).join('~')
-        if (!attachment) {
-            msg = "file not accepted"
-            res.status(401).json({ msg })
-            return
-        }
+        const id = Number(idDemande)
+        console.log(id);
+        
         let demand = await prisma.demand.findUnique({where:{id}})
         if(!demand){
             msg = "demand not found"
             res.status(404).json({msg})
             return
         }
-        fs.unlink(demand.attachment,err =>{
-            if(err){
-                msg = "file error !"
-                res.status(401).json({msg})
-                return
-            }
-        })
+        
+        // /**
+        //  * if the update add file ,add this with the two local 
+        //  * 
+        //  */
+        let  attachment :string[]
+        switch(files.length){
+            case 1: attachment = [...demand.attachment,files[0].buffer.toString('base64')]
+                    break
+            case 2: attachment = files.map(fic => fic.buffer.toString('base64'))
+                    break
+            default: attachment = [...demand.attachment]
+                    break
+        }
+        
+        if (!attachment) {
+            msg = "file not accepted"
+            res.status(401).json({ msg })
+            return
+        }
+        const createdAt = demand.createdAt
+
         demand = await prisma.demand.update({
             where:{id},
             data:{
-                ...req.body,
-                attachment
+                actDemand,
+                emailAdmin,
+                emailUser,
+                numActe,
+                province,
+                commune,
+                name,
+                firstName,
+                dateOfBirth,
+                placeOfBirth,
+                attachment,
+                createdAt,
+                status,
+                paid
             }
         })
-    }catch(err){
+        console.log(`the updated demand: ${demand}`);
         
+        msg = "update succesfully!"
+        res.status(201).json({msg,demand})
+        return
+    }catch(err){
+        res.status(501).json({err})
+        return
     }
 }
     export const notification = async (req:Request,res:Response) =>{
@@ -134,9 +174,9 @@ export const updateDemand = async (req:Request,res:Response) =>{
                 res.status(200).json({ msg })
                 return
             }
-            let data = demand.map(a => ({ ...a, attachment: a.attachment.split('~').map(att => process.env.API_URL!+PORT+'/image/'+att.replace('files/','') )}))
+            // let data = demand.map(a => ({ ...a, attachment: a.attachment.split('~').map(att => process.env.API_URL!+PORT+'/image/'+att.replace('files/','') )}))
             msg = "here the notification"
-            res.status(200).json({ msg, data })
+            // res.status(200).json({ msg, data })
             return
         } catch (err) {
             res.status(500).json({ err })
