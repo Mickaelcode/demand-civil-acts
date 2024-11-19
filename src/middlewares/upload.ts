0a4e5f10dc48d745722f8d9668870615b9e631d8
MickaelJoseph <mickaelandriana06@gmail.com>
2024-11-19 10:51:22 +0300

    test for limit storage

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,15 +1,6 @@
 import multer  from 'multer'
 
-const storage = multer.diskStorage({
-    destination:function (req,file,cb){
-        cb(null,'files/')
-    },
-    filename:function (req,file,cb){
-        // const extension = file.fieldname.split('.')
-        const date = Date.now()
-        cb(null,date+'-'+file.originalname)
-    }
+export const upload = multer({
+    limits:{fileSize:5 * 1024 * 1024},
+    storage:multer.memoryStorage()
 })
-
- export const upload = multer({storage})
-//  export default upload.array('files')=>
--- src/routes/acte.ts
+++ src/routes/acte.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { createAct, deleteActe, readAct, updateActe } from "../controllers/acte";
 import { security_auth } from "../middlewares/security";
 import { security_admin } from "../middlewares/security_admin";
-import multer from "multer";
+import { upload } from "../middlewares/upload";
+// import multer from "multer";
 
 const acteRoute = Router()
 
@@ -10,9 +11,9 @@ const acteRoute = Router()
  * here handle crud of acte
  */
 
-acteRoute.post('/create',security_auth,multer({}).array('files'),createAct)
+acteRoute.post('/create',security_auth,upload.array('files'),createAct)
 acteRoute.get('/lists',security_admin,readAct)
-acteRoute.put('/update',security_admin,multer({}).array('files'),updateActe)
+acteRoute.put('/update',security_admin,upload.array('files'),updateActe)
 acteRoute.delete('/delete',security_admin,deleteActe)
 
 export default acteRoute
